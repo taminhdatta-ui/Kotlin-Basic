6da6ce1188c7559f3c65193cb9b8dcee0db660e2
Minh Đạt <taminhdatta@gmail.com>
2026-09-21 11:15:17 +0700

    2nd commit

--- app/src/main/java/com/ute/studentprofile/MainActivity.kt
+++ app/src/main/java/com/ute/studentprofile/MainActivity.kt
@@ -9,14 +9,18 @@ import com.ute.studentprofile.databinding.ActivityMainBinding
 import com.ute.studentprofile.model.Student
 import com.ute.studentprofile.untils.toAcademicRanking
 import com.ute.studentprofile.untils.toast
+import android.content.Intent
+import android.net.Uri
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var currentStudent = Student(
-        id = "22505120005",
-        name = "Nguyen Van An",
-        className = "DD2026",
-        email = "anv@ute.udn.vn",
+        id = "2415053122107",
+        name = "Tạ Minh Đạt",
+        className = "24T1",
+        email = "tmdat@ute.udn.vn",
         gpa = 3.8
     )
 
@@ -41,6 +45,14 @@ class MainActivity : AppCompatActivity() {
             bindStudentData(currentStudent) // Vẽ lại dữ liệu mới lên Views
             toast("Cập nhật điểm thành công!")
         }
+        val btnCall = findViewById<Button>(R.id.btnCall)
+
+        btnCall.setOnClickListener {
+            val phoneNumber = "0912345678"
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:$phoneNumber")
+            startActivity(intent)
+        }
     }
 
     private fun bindStudentData(student: Student) {
@@ -51,5 +63,15 @@ class MainActivity : AppCompatActivity() {
                 "${student.gpa} GPA (${student.gpa.toAcademicRanking()})"
             edtNewGpa.setText (student.gpa.toString())
         }
+    }
+    private fun showConfirmDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Xóa hồ sơ")
+            .setMessage("Bạn có chắc chắn muốn xóa hồ sơ không?")
+            .setPositiveButton("Xóa") { _, _ ->
+                // Xử lý xóa hồ sơ
+            }
+            .setNegativeButton("Hủy", null)
+            .show()
     }
 }
